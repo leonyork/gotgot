97cd1abbf9abbeeafccd7dc7bd9fb7d91146dd0a
Leon York <leon@leonyork.com>
2018-10-20 23:39:36 +0100

    Install aws as part of build

--- CD/setup.sh
+++ CD/setup.sh
@@ -6,14 +6,16 @@ echo deleting stack
 aws cloudformation delete-stack --stack-name $stackname
 aws cloudformation wait stack-delete-complete --stack-name $stackname
 echo creating stack
-aws cloudformation create-stack --stack-name $stackname \
-    --template-url https://lambci.s3.amazonaws.com/templates/lambci.template \
+aws cloudformation create-stack --stack-name $stackname
+aws cloudformation create-change-set --stack-name $stackname \
+    --change-set-name $stackname \
+    --template-url https://lambci.s3.amazonaws.com/templates/template.yaml \
     --capabilities CAPABILITY_IAM \
     --parameters ParameterKey=GithubToken,ParameterValue=$1 \
     ParameterKey=Repositories,ParameterValue=leonyork/gotgot \
     ParameterKey=SlackChannel,ParameterValue="#gotgot" \
-    ParameterKey=SlackToken,ParameterValue=$2 \
-
-aws cloudformation wait stack-create-complete --stack-name $stackname
+    ParameterKey=SlackToken,ParameterValue=$2
+aws cloudformation execute-change-set --change-set-name $stackname
+aws cloudformation wait execute-change-set --change-set-name $stackname
 echo created stack
 npm install lambci-cli -g
